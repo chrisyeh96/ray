166e387e3d3a900589fcbd1621c545df7db99fa2
can <can@anyscale.com>
2023-09-07 01:26:41 +0000

    build ml ray image
    Signed-off-by: can <can@anyscale.com>

--- ci/ray_ci/test_docker_container.py
+++ ci/ray_ci/test_docker_container.py
@@ -8,12 +8,10 @@ from ci.ray_ci.docker_container import DockerContainer
 
 
 def test_run() -> None:
+    cmds = []
+
     def _mock_check_output(input: List[str]) -> None:
-        input_str = " ".join(input)
-        assert "ray-3.0.0.dev0-cp38-cp38-manylinux2014_x86_64.whl" in input_str
-        assert "rayproject/citemp:123-raypy38cu118base" in input_str
-        assert "requirements_compiled.txt" in input_str
-        assert "rayproject/ray:123456-py38-cu118" in input_str
+        cmds.append(" ".join(input))
 
     with mock.patch.dict(
         os.environ,
@@ -28,8 +26,21 @@ def test_run() -> None:
     ), mock.patch(
         "subprocess.check_output", side_effect=_mock_check_output
     ):
-        container = DockerContainer("py38")
+        container = DockerContainer("py38", "cu118", "ray")
         container.run()
+        cmd = cmds[-1]
+        assert "ray-3.0.0.dev0-cp38-cp38-manylinux2014_x86_64.whl" in cmd
+        assert "rayproject/citemp:123-raypy38cu118base" in cmd
+        assert "requirements_compiled.txt" in cmd
+        assert "rayproject/ray:123456-py38-cu118" in cmd
+
+        container = DockerContainer("py37", "cpu", "ray-ml")
+        container.run()
+        cmd = cmds[-1]
+        assert "ray-3.0.0.dev0-cp37-cp37m-manylinux2014_x86_64.whl" in cmd
+        assert "rayproject/citemp:123-ray-mlpy37cpubase" in cmd
+        assert "requirements_compiled_py37.txt" in cmd
+        assert "rayproject/ray-ml:123456-py37-cpu" in cmd
 
 
 if __name__ == "__main__":
